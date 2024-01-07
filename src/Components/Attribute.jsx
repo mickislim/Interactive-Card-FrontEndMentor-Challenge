f2d99f670a0313d95bdac8e8388c184ebe10ff26
mickislim <127298757+mickislim@users.noreply.github.com>
2024-01-07 14:33:02 -0600

    Add files via upload

--- src/Components/Attribute.jsx
+++ src/Components/Attribute.jsx
@@ -1,14 +1,12 @@
-
-
 import React from 'react'
 
 const Attribute = () => {
   return (
-    <div className="attribution-grid">
-       <div className="attribution">
-        Challenge by <a rel='noreferrer' href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-         Coded by <a href="#">Akinyemi Michael</a>.
-       </div></div>
+    <div className="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Akinyemi Michael</a>.
+  </div>
+
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,40 +1,72 @@
-
-
 import React from 'react'
-import back from '../images/bg-card-back.png'
-import logo from '../images/card-logo.svg'
+import cardBack from '../Assets/images/bg-card-back.png'
+import cardlogo from '../Assets/images/card-logo.svg'
+import { useGlobalContext } from '../Context'
 
-const Card = ({user,isUserActive,username, cardNumber,expMonth,expYear,cvc}) => {
+
+const Card = () => {
+const {user, isUserActive}=useGlobalContext()
   return (
-    
-    <div className="card-grid">
-      <div className="card-container">
-           <img src={back} id="img" alt="Card Back" />
-           <div className="cvc-number">
-             {isUserActive? <h4>{cvc}</h4> : <h4>000</h4>}
-           </div>
+    <section className="card">
+      
+  <div className="cardback">
+    <div className="card-positioning">
+      <div className="card-back-container">
+        <img src={cardBack} alt='cardback' className="card-back"/>
+
+      
+      </div>
+
+
+
+
+      <div className="card-front-bg">
+
+        <div className="logo-img">
+          <img src={cardlogo} alt='cardlogo' className="logo"/>
+        </div>
+ {isUserActive? (  <article > 
+
   
-          
-             <div className="front">
-               <div className="front-details">
-                 <div>
-                   <img src={logo} width="60px" alt="Card Logo" />
-                 </div>
+ <h1>{user.cardNumber} </h1> 
+
+<div className="user-detail"> 
+<h2>{user.cardHolder.toUpperCase()}</h2>
+<h2>{user.expMonth.length < 2? '0' + user.expMonth : user.expMonth}/{user.expYear.length < 2? '0' + user.expYear : user.expYear}</h2>
+
+</div>
+<div className="cvc-number">
+<h1>{user.cvcNumber}</h1> 
+
+</div>
+
+
+
+
+</article> ): (
+<article >
+  <h1>0000 0000 0000 0000</h1>
+
+<div className="user-detail"> <h2>JANE APPLESEED</h2>
+  <h2>00/00</h2></div>
+
+  <div className="cvc-number">
+    <h1>000</h1>
+  </div>
+   
   
-            <div className="front-number">
-                   {isUserActive? <h2>{cardNumber}</h2>   : <h2> 0000 0000 0000 0000</h2>}
-                 </div>
-                 <div className="front-text">
-                 {isUserActive ?  <h4>{username.toUpperCase()} </h4> : <h4>JANE APPLESEED</h4>}
-                  
-                   {isUserActive? <h4> {expMonth.length < 2? '0' + expMonth : expMonth} / {expYear.length < 2? '0' + expYear : expYear} </h4> : <h4>00 / 00 </h4> } 
-                  
-                 </div>
-               </div>
-             </div>
-         
-         </div>
+</article>)}
+ 
+
+
+      </div>
+
+  
+
     </div>
+   
+  </div>
+</section>
   )
 }
 
